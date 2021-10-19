b0fcb50e48d02dac94c4e27d634868b096980b65
Christian Ulrich <git@chris.ullihome.de>
2021-10-19 21:43:39 +0200

    fix html

---
+++ export.py
@@ -0,0 +1,12 @@
+import sys,pathlib;sys.path.append(str(pathlib.Path(__file__).parent.parent))
+from blender_export import *
+ExportObject('space/trappist1a.blend','Collection','space/trappist-1a')
+ExportObject('space/trappist1b.blend','Collection','space/trappist-1b')
+ExportObject('space/trappist1c.blend','Collection','space/trappist-1c')
+ExportObject('space/trappist1d.blend','Collection','space/trappist-1d')
+ExportObject('space/trappist1e.blend','Collection','space/trappist-1e')
+ExportObject('space/trappist1f.blend','Collection','space/trappist-1f')
+ExportObject('space/trappist1g.blend','Collection','space/trappist-1g')
+
+ExportObject('space/singlepanel.blend','Collection','space/sycia_panel')
+ExportObject('space/sycia_body.blend','Collection','space/sycia_body')
